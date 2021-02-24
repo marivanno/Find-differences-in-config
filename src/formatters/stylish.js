7cf6508c61b44eb798e2981632a54d27af8f6a6e
Ivan Shadrin <chubakaivan@gmail.com>
2021-02-24 17:57:00 +0300

    123

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,5 +1,32 @@
 import _ from 'lodash';
-import { spaceGen, ObjectToString, generateToString } from '../utiliy.js';
+
+const spaceGen = (num) => '    '.repeat(num);
+
+const buildArray = (object) => {
+  const ownArray = Object.entries(object);
+  return ownArray.map(([key, value]) => {
+    if (_.isObjectLike(value)) {
+      return [key, buildArray(value)];
+    }
+    return [key, value];
+  });
+};
+
+const buildTree = (array, depth = 0) => {
+  const result = array.reduce((acc, item) => {
+    if (Array.isArray(item[1])) {
+      return `${acc} \n${spaceGen(depth)}  ${item[0]}: ${buildTree(item[1], depth + 1)}`;
+    }
+    return `${acc} \n${spaceGen(depth)}  ${item[0]}: ${item[1]}`;
+  }, '{');
+  return `${result} \n${spaceGen(depth)}}`;
+};
+
+const generateToString = (depth, key, value, char) => {
+  if (_.isObjectLike(value)) {
+    return `${spaceGen(depth)}${char} ${key}: ${buildTree(buildArray(value), 2)}`;
+  } return `${spaceGen(depth)}${char} ${key}: ${value}`;
+};
 
 const renderTree = (data, depth = 0) => {
   const result = data.reduce((acc, {
