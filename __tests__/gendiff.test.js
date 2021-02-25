002e50986c64df965e9e1913c48d7b30e860ff14
Ivan Shadrin <chubakaivan@gmail.com>
2021-02-25 18:34:56 +0300

    some chenges

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,6 +2,11 @@ import path from 'path';
 import fs from 'fs';
 import formatter from '../src/formatters/index.js';
 import buildAst from '../src/index.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 test.each([
   ['file1.json', 'file2.json', 'plain'],
@@ -11,11 +16,11 @@ test.each([
   ['file5.json', 'file6.json', 'plain'],
   ['file5.json', 'file6.json', 'stylish'],
 ])('gendiff', (before, after, format) => {
-  const file1 = path.resolve('', `__tests__/__fixtures__/${before}`);
-  const file2 = path.resolve('', `__tests__/__fixtures__/${after}`);
+  const file1 = path.resolve(__dirname, `__tests__/__fixtures__/${before}`);
+  const file2 = path.resolve(__dirname, `__tests__/__fixtures__/${after}`);
   const ast = buildAst(file1, file2);
   fs.writeFileSync(`c:/dev/${format}_${before}_${after}_result`, formatter(ast, format), 'utf8');
-  const result = fs.readFileSync(path.resolve('', `__tests__/__fixtures__/${format}_${before}_${after}_result`), 'utf8');
+  const result = fs.readFileSync(path.resolve(__dirname, `__tests__/__fixtures__/${format}_${before}_${after}_result`), 'utf8');
   expect(formatter(ast, format)).toBe(result);
 });
 
